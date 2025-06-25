ea6819bcdca9e9e8e419df3b05f79b213f372b6c
Gustavo-CS <gustavocarvalho9340@gmail.com>
2025-06-25 11:54:56 -0300

    buscando atendimentos dos agendamentos

--- src/app/api/historico/[id]/route.js
+++ src/app/api/historico/[id]/route.js
@@ -1,16 +1,53 @@
 import db from "@/infra/database";
-import { profissional, cliente, usuario } from "@root/drizzle/schema";
+import { profissional, cliente, usuario, agendamentos, disponibilidade } from "@root/drizzle/schema";
 import { eq } from "drizzle-orm";
+import { alias } from 'drizzle-orm/sqlite-core'; // ou 'drizzle-orm/pg-core' se for PostgreSQL
 
-export async function GET(request) {
+export async function GET(request, { params }) {
   try {
+    const id = params.id;
     const { searchParams } = new URL(request.url);
     const tipo = searchParams.get('tipo');
 
-    if (tipo === "cliente") {
-      //falta melhorar as tabelas pra fazer a busca
-    } else {
+    const usuarioCliente = alias(usuario, 'usuarioCliente');
+    const usuarioProfissional = alias(usuario, 'usuarioProfissional');
 
+    let atendimentos = [];
+    if (tipo === "cliente") {
+      const clienteRaw = await db
+        .select()
+        .from(agendamentos)
+        .innerJoin(cliente, eq(cliente.id, agendamentos.clienteId))
+        .innerJoin(usuarioCliente, eq(usuarioCliente.id, cliente.usuarioId))
+        .innerJoin(disponibilidade, eq(disponibilidade.id, agendamentos.disponibilidadeId))
+        .innerJoin(profissional, eq(profissional.id, disponibilidade.profissionalId))
+        .innerJoin(usuarioProfissional, eq(profissional.usuarioId, usuarioProfissional.id))
+        .where(eq(agendamentos.clienteId, id))
+      atendimentos = clienteRaw.map((row) => ({
+        ...row,
+        tipo: 'cliente',
+      }));
+    } else {
+      const profissionalRaw = await db
+        .select()
+        .from(agendamentos)
+        .innerJoin(cliente, eq(cliente.id, agendamentos.clienteId))
+        .innerJoin(usuarioCliente, eq(usuarioCliente.id, cliente.usuarioId))
+        .innerJoin(disponibilidade, eq(disponibilidade.id, agendamentos.disponibilidadeId))
+        .innerJoin(profissional, eq(profissional.id, disponibilidade.profissionalId))
+        .innerJoin(usuarioProfissional, eq(profissional.usuarioId, usuarioProfissional.id))
+        .where(eq(disponibilidade.profissionalId, id))
+      atendimentos = profissionalRaw.map((row) => ({
+        ...row,
+        tipo: 'profissional',
+      }));
+    }
+
+    if (atendimentos.length > 0) {
+      return new Response(JSON.stringify(atendimentos), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
     }
     return new Response(null, { status: 204 });
   } catch (error) {
